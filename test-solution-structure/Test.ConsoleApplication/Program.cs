a6dd79c8d387be5d30d53fe39afe5b1c28d4cd8a
mbfjpv <mbfjpv@gmail.com>
2016-11-21 21:15:27 +0530

    Initial test solution structure test

---
+++ test-solution-structure/Test.ConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ClassLibrary.Class1().Print();
+            Console.ReadLine();
+        }
+    }
+}
