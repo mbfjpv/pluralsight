b635651fbbc5ac9326eba76489b23bdbab027c58
mbfjpv@gamil.com <mbfjpv@gamil.com>
2016-11-23 09:32:14 +0530

    Added "c-sharp-fundamentals-with-visual-studio-2015" project

---
+++ c-sharp-fundamentals-with-visual-studio-2015/Grades.Tests/GradesBookTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradesBookTest
+    {
+        [TestMethod]
+        public void TestName()
+        {
+            GradesBook gb = new GradesBook();
+            gb.Name = "ABC";
+            Assert.AreEqual(gb.Name, "ABC");
+        }
+    }
+}
---
+++ c-sharp-fundamentals-with-visual-studio-2015/Grades/GradesBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradesBook
+    {
+        private string _name = string.Empty;
+
+        public string Name {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if(!String.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+            }
+        }
+
+    }
+}
