6b8cfdd17330d5ec50bcbaff2f5c2c46f9c57b70
mbfjpv@gamil.com <mbfjpv@gamil.com>
2016-11-30 09:29:39 +0530

    Added Employee Comparer

--- csharp-generics/CSharp.Generics.Console/Program.cs
+++ csharp-generics/CSharp.Generics.Console/Program.cs
@@ -4,12 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharp.Generics.Console
+namespace CSharp.Generics.ConsoleApp
 {
     class Program
     {
+        class Employee
+        {
+            public string Name { get; set; }
+        }
+
+        class EmployeeComparer : IEqualityComparer<Employee>, 
+                                    IComparer<Employee>
+        {
+            public int Compare(Employee x, Employee y)
+            {
+                return String.Compare(x.Name, y.Name);
+            }
+
+            public bool Equals(Employee x, Employee y)
+            {
+                return String.Equals(x.Name, y.Name);
+            }
+
+            public int GetHashCode(Employee obj)
+            {
+                return obj.Name.GetHashCode();
+            }
+
+
+        }
+
         static void Main(string[] args)
         {
+            EmployeeTest1();
+        }
+
+        private static void EmployeeTest1()
+        {
+            var departments = new SortedDictionary<string, SortedSet<Employee>>();
+
+            departments.Add("Sales", new SortedSet<Employee>(new EmployeeComparer()));
+            departments["Sales"].Add(new Employee { Name = "Scott" });
+            departments["Sales"].Add(new Employee { Name = "Dani" });
+            departments["Sales"].Add(new Employee { Name = "Dani" });
+
+            departments.Add("Engineering", new SortedSet<Employee>(new EmployeeComparer()));
+            departments["Engineering"].Add(new Employee { Name = "Scott" });
+            departments["Engineering"].Add(new Employee { Name = "Alex" });
+            departments["Engineering"].Add(new Employee { Name = "Dani" });
+
+            foreach (var department in departments)
+            {
+                Console.WriteLine(department.Key);
+                foreach(var employee in department.Value)
+                {
+                    Console.WriteLine("\t" + employee.Name);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
